812b4e528dd7c83bad454bf5ce79c9007e7dbcca
‘miklik99’ <miklik.martin01@gmail.com>
2019-11-17 23:40:14 +0100

    prvni cast

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logoTmave.png';
+import people from './images/people.png';
+import './App.sass';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="landing">
+      <img src={logo} className="App-logo" alt="logo" />
+        <nav>
+            <a href="#" className="hidden">Info</a>
+            <a href="#">Kontakt</a>
+        </nav>
+        <p className="uvodniText">ahoj, kompletni web pro vas zatim pripravujeme, zatim lorem ipsum</p>
+        <div className="seskupeni">
+          <p>letos to vypukne<br /></p>
+          <p className="datum">7. 5. 2020</p>
+          <img src={people} className="people" alt="people" />
+        </div>
+      </section>
+      <section className="info">
+        <p>Majales 2019 v cislech</p>
+        <div className="grid">
+          <div className="sekce1">
+            <p>Majales 2019 v cislech</p>
+            <p>Majales 2019 v cislech</p>
+            <p>Majales 2019 v cislech</p>
+            <p>Majales 2019 v cislech</p>
+          </div>
+          <div className="sekce2">
+            <p>Majales 2019 v cislech</p>
+            <p>Majales 2019 v cislech</p>
+            <p>Majales 2019 v cislech</p>
+            <p>Majales 2019 v cislech</p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
